bb1d1b2f7cfc8f289f4c1c937551bad2e0cb0d61
Anastasiia Bondarenko <ne7sgreen@gmail.com>
2023-05-08 17:15:01 +0300

    Add SSO using JWT

---
+++ graphql/src/main/java/com/news/graphql/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.news.graphql.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.authorizeHttpRequests()
+      .anyRequest().fullyAuthenticated()
+      .and()
+      .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      .and()
+      .oauth2ResourceServer().jwt()
+      .and()
+      .and()
+      .cors().and().csrf().disable();
+    return http.build();
+  }
+}
